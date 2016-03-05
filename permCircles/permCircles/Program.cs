5d70a546e5a0c0858b1f6f50a86965575ad8e654
jkbits1 <jkbits1@users.noreply.github.com>
2016-03-05 05:26:11 +0000

    implementation of turnWheel

---
+++ permCircles/permCircles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace permCircles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] pos = {1,2,3,4};
+            List<int> first = new List<int>(pos);
+
+            var tPart = turnWheel(first, 1);
+
+            int i = 0;
+
+
+        }
+
+        static List<int> turnWheel(List<int> wheelPos, int chunk) {
+            IEnumerable<int> takePart = wheelPos.Take(chunk);
+            IEnumerable<int> dropPart = wheelPos.Skip(chunk);
+
+            IEnumerable<int> newPos = dropPart.Concat(takePart);
+
+            return newPos.ToList();
+        }
+
+    }
+
+    
+}
